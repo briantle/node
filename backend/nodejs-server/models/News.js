b6d7285fa9eea510774a5dfe0c4f7da678050b0a
briantle <31326217+briantle@users.noreply.github.com>
2020-04-14 10:17:24 -0500

    Added post for user route, edited news model to include url imgurl and other attributes

--- backend/nodejs-server/models/News.js
+++ backend/nodejs-server/models/News.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 
 const NewsSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    description: { type: String, required: true }
+    description: { type: String, required: true },
+    url: {type: String, required: true},
+    imgurl: {type: String, required: true},
+    publishedAt: {type: Date, required: true}
 }, {
     timestamps: true
 })
