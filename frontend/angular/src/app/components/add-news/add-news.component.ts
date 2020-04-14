f528f04f191c92b49a4b12e1bea426708ad9644d
msdrouil <msdrouil@gmail.com>
2020-04-14 14:06:42 -0400

    changes

---
+++ frontend/angular/src/app/components/add-news/add-news.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/_services/news.service';
+import { News } from 'src/app/models/news';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-add-news',
+  templateUrl: './add-news.component.html',
+  styleUrls: ['./add-news.component.css']
+})
+export class AddNewsComponent implements OnInit {
+
+  title: string = '';
+	description: string = '';
+	url: string = '';
+	urlToImage: string = '';
+  publishedAt: string = '';
+
+  constructor(private newsService: NewsService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  addNewNews() {
+    const data = {
+      title: this.title,
+      description: this.description,
+      url: this.url,
+      urlToImage: this.urlToImage,
+      publishedAt: this.publishedAt
+    }
+    console.log(data);
+    this.newsService.addNews(data).subscribe(() => {
+      this.router.navigate(['/signin'])
+    },
+    () => {
+      console.log("Failed to add news")
+    })
+
+  }
+
+}
