ec259ae9a6af133b797301ce9f167e47f356240d
Saishree Bhupathi <saishree.bhupathi@gmail.com>
2020-04-14 12:31:26 -0400

    Login and Register

---
+++ frontend/angular/src/app/_helpers/index.ts
@@ -0,0 +1,4 @@
+export * from './fake-backend';
+export * from './auth.gard'
+export * from './jwt.interceptor';
+export * from './error.interceptor';
---
+++ frontend/angular/src/app/app.routing.ts
@@ -0,0 +1,18 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './_helpers';
+
+
+const routes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
