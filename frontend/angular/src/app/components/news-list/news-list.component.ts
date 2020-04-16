6160e5869086b092049b304c848a83d6d67fa708
briantle <31326217+briantle@users.noreply.github.com>
2020-04-15 20:39:03 -0500

    got authorization done

--- frontend/angular/src/app/components/news-list/news-list.component.ts
+++ frontend/angular/src/app/components/news-list/news-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { News } from 'src/app/models/news';
 import { NewsService } from 'src/app/_services/news.service';
 import { HttpClient } from '@angular/common/http';
+import { AuthenticationService } from 'src/app/_services';
+import { Router } from '@angular/router';
 
 const apiUrl = 'http://localhost:4000/news'
 
@@ -19,20 +21,26 @@ export class NewsListComponent implements OnInit {
 
  
 
-  constructor(private newsService: NewsService, private httpService: HttpClient) { }
+  constructor(private auth: AuthenticationService, private newsService: NewsService, private httpService: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
 
-    
-    this.httpService.get(apiUrl + "/").subscribe(
-      data => {
-        this.news = data as string [];	 
-      },
-      () => {
-        console.log('cant find data')
-        })
-        
-    }   
+    if (localStorage.getItem("currentUser").split('"')[3] != "admin"){
+      alert("Unauthorized")
+      this.router.navigate(["/"])
+    }
+    else
+    {
+      console.log("authorized")
+      this.httpService.get(apiUrl + "/").subscribe(
+        data => {
+          this.news = data as string [];	 
+        },
+        () => {
+          console.log('cant find data')
+          })
+    }
+  }   
 
 
 
